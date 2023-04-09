88da173daa8352965c0ffe43cd1cdef6e0d0da8b
David Perelman <dperelman3@gmail.com>
2023-04-10 00:49:04 +0300

    Dashboard: delete post functionality

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,15 @@
 import Message from '@/components/Message';
 import { auth, db } from '@/utils/firebase';
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where,
+} from 'firebase/firestore';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { BsTrash2Fill } from 'react-icons/bs';
 import { AiFillEdit } from 'react-icons/ai';
@@ -12,23 +19,29 @@ export default function Dashboard() {
   const [user, loading] = useAuthState(auth);
   const [posts, setPosts] = useState([]);
 
+  // See if user is logged
+  const getData = useCallback(async () => {
+    if (loading) return;
+    if (!user) return route.push('/auth/login');
+    const collectionRef = collection(db, 'posts');
+    const q = query(collectionRef, where('user', '==', user.uid));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+
+    return unsubscribe;
+  }, [loading, route, user]);
+
+  // Delete post
+  const deletePost = async (id) => {
+    const docRef = doc(db, 'posts', id);
+    await deleteDoc(docRef);
+  };
+
   // Get users data
   useEffect(() => {
-    // See if user is logged
-    const getData = async () => {
-      if (loading) return;
-      if (!user) return route.push('/auth/login');
-      const collectionRef = collection(db, 'posts');
-      const q = query(collectionRef, where('user', '==', user.uid));
-      const unsubscribe = onSnapshot(q, (snapshot) => {
-        setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      });
-
-      return unsubscribe;
-    };
-
     getData();
-  }, [user, loading, route]);
+  }, [getData]);
 
   return (
     <div>
@@ -38,7 +51,10 @@ export default function Dashboard() {
           return (
             <Message key={post.id} {...post}>
               <div className='flex gap-4'>
-                <button className='text-pink-600 flex items-center justify-center gap-2 py-2 text-sm'>
+                <button
+                  onClick={() => deletePost(post.id)}
+                  className='text-pink-600 flex items-center justify-center gap-2 py-2 text-sm'
+                >
                   <BsTrash2Fill className='text-2xl' /> Delete
                 </button>
                 <button className='text-teal-600 flex items-center justify-center gap-2 py-2 text-sm'>
