a688deece050b10594870b23edcb6ca5240c7661
David Perelman <dperelman3@gmail.com>
2023-04-09 20:35:11 +0300

    setup Message.js

---
+++ components/Message.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Message({ children }) {
+  return (
+    <div className='bg-white p-8 border-b-2 rounded-lg'>
+      <div className='flex items-center'>
+        <Image src='' alt='' />
+        <h2>user</h2>
+      </div>
+      <div>
+        <p>description</p>
+      </div>
+      {children}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Message from '@/components/Message';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,7 +8,11 @@ export default function Home() {
         <title>Creative Minds</title>
         <meta name='description' content='Creative Minds' />
       </Head>
-      <main></main>
+
+      <div className='my-12 text-lg font-medium'>
+        <h2>See what other people are saying</h2>
+        <Message />
+      </div>
     </div>
   );
 }
