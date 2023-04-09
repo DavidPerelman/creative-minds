628d24c09853b3deac01629c147500e23e6330bb
David Perelman <dperelman3@gmail.com>
2023-04-09 19:17:15 +0300

    Dashboard: change user loggedout UI

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,23 @@
 import { auth } from '@/utils/firebase';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Dashboard() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+
+  // See if user id logged
+  const getData = async () => {
+    if (loading) return;
+    if (!user) return route.push('/auth/login');
+  };
+
+  // Get users data
+  useEffect(() => {
+    getData();
+  }, [user, loading, getData]);
+
   return (
     <div>
       <h1>Your Posts</h1>
